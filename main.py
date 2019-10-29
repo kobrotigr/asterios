72c1455a96376afd136ee0ee5577c27a05a85236
User <user@MHALAUCHAK>
2019-10-29 10:39:51 +0300

    added sound alert

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
 import time
 
 import requests
+from pygame import mixer
 from bs4 import BeautifulSoup
 
 
@@ -10,13 +10,17 @@ def main():
         r = requests.get('https://asterios.tm/index.php?cmd=rss&serv=8&filter=keyboss')
         bs = BeautifulSoup(r.text, 'html.parser')
         table = bs.find('table', width=650)
-        if 'Death Lord Hallate' in table.prettify():
-            # your alert here!
-            print('yes')
+        boss_name = 'Death Lord Hallate'
+        if boss_name in table.prettify():
+            mixer.init()
+            mixer.music.load('sound.mp3')
+            mixer.music.play()
+            print('boss has been killed: ', boss_name)
 
         a = table.find('a')
-        print('last boss was:', a.next_element)
+        print('last killed boss was:', a.next_element)
         time.sleep(6)
+
 
         # ========================================================================
         # os.system('aplay sound.mp3')
